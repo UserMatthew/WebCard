383b0491e2366550f7e903bdcf65a71582ec78d6
Matt <matthew04@hotmail.es>
2024-05-09 16:07:01 -0500

    primeras funtion en JS

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,22 @@
-let nombre = document.getElementById('nick');
+let nombre 
+let nickName;
+
+
+function iniciarWeb () {
+    nombre = document.getElementById('nick');
+    nombre.addEventListener('click', function (event) {
+        nombre.value = '';
+        nombre.focus();
+    });
+
+    nombre.addEventListener('keypress', function(event) {
+        if (event.key === "Enter") {
+            nickName = nombre.value;
+            alert("El nombre o nick ingresado es: " + nickName);
+            event.preventDefault();
+        }
+     });
+    
+}
+
+window.addEventListener('load', iniciarWeb)
